b87da97c5eb70174681986a575880a0ec6d2de79
Mykhailo Boiko <mboiko.prog@gmail.com>
2023-02-13 22:10:38 +0200

    sprint01: t09: add func that print any string
    Signed-off-by: Mykhailo Boiko <mboiko.prog@gmail.com>

---
+++ sprint01/t09/mx_strlen.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+int mx_strlen(const char *s) {
+    int ret = 0;
+
+    if (s != NULL) {
+        while (s[ret] != '\0') {
+            ret++;
+        }
+    }
+
+    return ret;
+}
