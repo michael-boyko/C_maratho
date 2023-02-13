39bcfc2a13382f407038a86b1be40f6be4b7f264
Mykhailo Boiko <mboiko.prog@gmail.com>
2023-02-13 22:09:47 +0200

    sprint01: t08: fix strlen
    Signed-off-by: Mykhailo Boiko <mboiko.prog@gmail.com>

--- sprint01/t08/mx_strlen.c
+++ sprint01/t08/mx_strlen.c
@@ -4,8 +4,11 @@ int mx_strlen(const char *s) {
     int ret = 0;
 
     if (s != NULL) {
-        while (s[ret++] != '\0');
+        while (s[ret] != '\0') {
+            ret++;
+        }
     }
 
     return ret;
 }
+
