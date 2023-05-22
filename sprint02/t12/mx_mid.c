56247e5c36082d5813f96304a7cd419a8f48765b
michael-boyko <kozak.miksa@gmail.com>
2023-05-22 20:57:21 +0300

    S02:T12: fix mid chose errors
    Signed-off-by: michael-boyko <kozak.miksa@gmail.com>

--- sprint02/t12/mx_mid.c
+++ sprint02/t12/mx_mid.c
@@ -1,9 +1,9 @@
 int mx_mid(int a, int b, int c) {
     int ret = a;
 
-    if (b > a && b < c) {
+    if (b > a && b < c || b > c && b < a) {
         ret = b;
-    } else if (c > a && c < b) {
+    } else if (c > a && c < b || c < a && c > b) {
         ret = c;
     }
 
