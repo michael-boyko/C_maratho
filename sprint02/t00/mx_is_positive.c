981d45af94f4ae70e727c59032f447e6df99fc2b
michael-boyko <kozak.miksa@gmail.com>
2023-05-22 20:54:40 +0300

    S02:T00: add minor change to mx_is_positive
    Signed-off-by: michael-boyko <kozak.miksa@gmail.com>

--- sprint02/t00/mx_is_positive.c
+++ sprint02/t00/mx_is_positive.c
@@ -2,6 +2,6 @@ void mx_is_positive(int i) {
     if (i == 0) {
         mx_printstr("zero\n");
     } else {
-        mx_printstr(i > 0 ? "positiv\n" : "negative\n");
+        mx_printstr(i > 0 ? "positive\n" : "negative\n");
     }
 }
